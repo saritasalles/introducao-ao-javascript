4d7f3d5b60f5c98dee86158b428b3c6a05a961f7
Sarita Salles <107811869+saritasalles@users.noreply.github.com>
2022-08-29 23:20:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0;
 
 function increment() {
-currentNumber = currentNumber + 1;
-currentNumberWrapper.innerHTML = currentNumber;
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
 }
 
 function decrement() {
